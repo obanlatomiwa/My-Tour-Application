dcda597522dfa6899d22d7a3271c530b000adeb6
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-07-11 05:54:11 +0100

    requests

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
+// core modules
+const fs = require('fs');
+
+// 3rd-party modules
 const express = require('express');
 
 // setting up express app
 const app = express();
 
-// setting up server
+// app middleware
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send(`listening on port ${port}`);
+// loading data
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf8'));
+
+// setting up server
+// get method
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+// post method
+app.post('/api/v1/tours', (req, res) => {
+  newID = tours[tours.length - 1].id + 1;
+  newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+
+  // push to file
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
 });
 
 const port = 8000;
 
 app.listen(port, () => {
-  console.log('hello world');
+  console.log(`listening on port ${port}`);
 });
