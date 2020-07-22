65f9c54fc8fa6b9fc0e0aa161bc284681decea4f
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-07-22 21:58:20 +0100

    modified the tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,27 @@ const Tour = require('../models/tourModel');
 // route handlers for tours
 exports.getTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // mongo db way
+    // const tours = await Tour.find({ difficulty: 'easy', duration: 5 });
+
+    // mongoose way
+    // const tours = await Tour.find()
+    //   .where('difficulty')
+    //   .equals('easy')
+    //   .where('duration')
+    //   .equals(5);
+
+    // build query
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const queryObj = { ...req.query };
+    const excludeFields = ['sort', 'page', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    const query = Tour.find(queryObj);
+
+    // execute query
+    const tours = await query;
+
+    // send response
     res.status(200).json({
       status: 'success',
       results: tours.length,
