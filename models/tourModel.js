44bc3a36c14859d639681ec79bbf73548c76dfde
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-07-22 16:20:52 +0100

    added more features to the tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,53 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     unique: true,
+    trim: true,
     required: [true, 'name is required'],
   },
-  rating: {
+  duration: {
+    type: String,
+    required: [true, 'duration is required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'group size required'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'difficulty required'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'price is required'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'summary is required'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'an image cover is required'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
