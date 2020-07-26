2a4769510c7f6b3085c673cd14c0ba38264c47be
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-07-26 05:55:36 +0100

    implemented pagination for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,8 +34,28 @@ exports.getTours = async (req, res) => {
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
-      
     }
+
+    // 4) field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 5) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page doesn't exist");
+    }
+
     // execute query
     const tours = await query;
 
