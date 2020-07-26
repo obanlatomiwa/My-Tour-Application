d93f339d72119a1f62c574df3c16b0af6002a595
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-07-26 21:20:13 +0100

    added tour model virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,59 +1,74 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: [true, 'name is required'],
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: [true, 'name is required'],
+    },
+    duration: {
+      type: String,
+      required: [true, 'duration is required'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'group size required'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'difficulty required'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'price is required'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'summary is required'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'an image cover is required'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: String,
-    required: [true, 'duration is required'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'group size required'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'difficulty required'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'price is required'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'summary is required'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'an image cover is required'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// Document Middleware
+tourSchema.pre('save', function () {
+  console.log(this);
 });
 
+// virtual properties
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -8,7 +8,13 @@ const router = express.Router();
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router.route('/').get(tourController.getTours).post(tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
