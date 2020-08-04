590248622c642a181238dbb5c8caa02a04dd8890
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-08-04 22:41:18 +0100

    added signup route with tokenization

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,21 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsyncError = require('../utils/catchAsyncError');
 
 exports.signup = catchAsyncError(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // token
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
+    token,
     status: 'success',
     data: {
       user: newUser,
