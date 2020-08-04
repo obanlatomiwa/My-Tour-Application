cbab2c935cb3905ce36473204e913798ada576d6
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-08-04 02:44:14 +0100

    handled non-existing url response

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// main express application
+
 // core modules
 const fs = require('fs');
 
@@ -26,5 +28,13 @@ app.use(express.static(`${__dirname}/public`));
 // mounting routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// handling non-existing routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
