7a11e1f49c8650431d310a250300d21c1a581c4f
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-08-07 16:45:09 +0100

    added cookies functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,22 @@ const signToken = (id) => {
 // send token
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  // send cookie to the browser
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt-token', token, cookieOptions);
+
+  // remove password from the output
+  user.password = undefined;
+
   res.status(statusCode).json({
     token,
     status: 'success',
