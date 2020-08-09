09d9eca9bf5072e8fc863bac3b30c86df0383a38
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-08-09 17:53:13 +0100

    now users can not make more than 1 reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -36,6 +36,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Ratings must be above 1.0'],
       max: [5, 'Ratings must be below 5.0'],
+      set: val => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
