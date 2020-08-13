fed176016092a8b9253c44a97c5b16e91bf99907
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-08-13 20:39:19 +0100

    users can now update their details from the profile page

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,14 @@
 import '@babel/polyfill';
 import displayMap from './mapBox';
 import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 //DOM
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutButton = document.querySelector('.nav__el--logout');
+const updateUserDataForm = document.querySelector('.form-user-data');
+const updateUserPassword = document.querySelector('.form-user-settings');
 
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
@@ -25,8 +28,20 @@ if (loginForm) {
 }
 
 if (logOutButton) logOutButton.addEventListener('click', logout);
- 
 
+if (updateUserDataForm)
+  updateUserDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
 
-
-
+  if (updateUserPassword)
+  updateUserDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const passwordCurrent = document.getElementById('name').value;
+    const password = document.getElementById('name').value;
+    const passwordConfirm = document.getElementById('email').value;
+    updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+  });
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
+import { showAlert } from './alert';
 
-export const updatedUser = async (name, email) => {
-    const updatedUser = await 
-}
+// type is either data(name and email) or password
+export const updateSettings = async (data, type) => {
+  const url = type === 'password' ? 'updateMyPassword' : 'updateMe';
+  try {
+    const result = await axios({
+      method: 'PATCH',
+      url: `http://127.0.0.1:8000/api/v1/users/${url}`,
+      data,
+    });
+    if (result.data.status === 'success') {
+      showAlert(
+        'success',
+        `Your ${type.toUpperCase()} has been successfully updated`
+      );
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
